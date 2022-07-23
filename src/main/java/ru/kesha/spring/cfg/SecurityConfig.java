d4929ec0bd2eeff79133267c2d17e364732e24f4
Innokenty2009 <chtoblinxexe@gmail.com>
2022-07-23 21:12:45 +0300

    second

--- src/main/java/ru/kesha/spring/cfg/SecurityConfig.java
+++ src/main/java/ru/kesha/spring/cfg/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http.authorizeRequests()
                 .antMatchers("/").hasAnyRole("EMPLOYEE, HR, MANAGER")
                 .antMatchers("/hr_info").hasAnyRole("HR, MANAGER")
